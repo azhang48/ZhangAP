46c3b1b2a8328e143a5f674528b4818ab9118f7b
azhang48 <azhang@sageridge.org>
2022-08-29 11:51:30 -0700

    added comments

--- InClassAssignments/ICA1.java
+++ InClassAssignments/ICA1.java
@@ -4,10 +4,15 @@ public class ICA1
 {
     public static void main(String[] args)
     {
+        //Declare variables
         double teaspoonsIn;
         Scanner askForTeaspoons = new Scanner(System.in);
+
+        //Get User Input
         System.out.print("Enter the amount in teaspoons: ");
         teaspoonsIn = askForTeaspoons.nextDouble();
+
+        //Print Results
         System.out.println('\n' + teaspoonsIn + " teaspoons is:");
         System.out.println(teaspoonsIn/3 + " Tablespoons");
         System.out.println(teaspoonsIn/48 + " Cups");
